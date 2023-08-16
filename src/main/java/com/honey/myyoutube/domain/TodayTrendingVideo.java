4f4de4d19f9f595decd383ddb43cbb41aaed14e4
hingjae <ljh23270@naver.com>
2023-08-16 16:19:55 +0900

    todaytrendingvideo 엔티티 추가

---
+++ src/main/java/com/honey/myyoutube/domain/TodayTrendingVideo.java
@@ -0,0 +1,33 @@
+package com.honey.myyoutube.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@ToString(callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "today_trending_video")
+@Entity
+public class TodayTrendingVideo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "video_id")
+    private Video video;
+
+    private LocalDateTime trendTime;
+
+    private int score;
+
+    @Builder
+    private TodayTrendingVideo(Long id, Video video, LocalDateTime trendTime, int score) {
+        this.id = id;
+        this.video = video;
+        this.trendTime = trendTime;
+        this.score = score;
+    }
+}
